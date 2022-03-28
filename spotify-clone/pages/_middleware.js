611f21cc0453c7ad184e9e1f7c3c7a7b3e1c406d
Stephen Ssendikadiwa <stephen@Apples-MacBook-Air.local>
2022-03-28 17:15:02 +0300

    Implement authentication middleware

---
+++ spotify-clone/pages/_middleware.js
@@ -0,0 +1,27 @@
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+
+export async function middleware(req) {
+	// token will exist if user is logged in
+	const token = await getToken({ req, secret: process.env.JWT_SECRET });
+
+	const { pathname } = req.nextUrl;
+
+	// All the request if the following is true
+	// 1) Its request for next-auth session & provider fetching
+	// 2) the token exists
+	if (pathname.includes("/api/auth") || token) {
+		return NextResponse.next();
+	}
+	// Redirect them to login if they dont have token AND are requesting a protected route
+	if (!token && pathname !== "/login") {
+		const url = req.nextUrl.clone();
+		url.pathname = "/login";
+		return NextResponse.redirect(url);
+	}
+	if (token && pathname.includes("/login")) {
+		const url = req.nextUrl.clone();
+		url.pathname = "/";
+		return NextResponse.redirect(url);
+	}
+}
